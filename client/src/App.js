c7b74ea4b7c9bfcc944b0aa9042335b02e8e0f8f
Nika-Firada <nika.firada@gmail.com>
2023-07-02 17:01:39 +0400

    damn

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import LongPulling from "./LongPulling";
+import EventSourcing from "./EventSourcing";
+import WebSock from "./WebSock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <LongPulling /> */}
+      {/* <EventSourcing /> */}
+      <WebSock />
+    </>
   );
 }
 
---
+++ client/src/WebSock.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react';
+import axios from "axios";
+import uniqid from 'uniqid';
+
+const WebSock = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+    const socket = useRef()
+    const [connected, setConnected] = useState(false);
+    const [username, setUsername] = useState('')
+
+    function connect() {
+        socket.current = new WebSocket('ws://localhost:5000')
+
+        socket.current.onopen = () => {
+            setConnected(true)
+            const message = {
+                event: 'connection',
+                username,
+                id: uniqid()
+            }
+            socket.current.send(JSON.stringify(message))
+        }
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data)
+            setMessages(prev => [message, ...prev])
+        }
+        socket.current.onclose = () => {
+            console.log('Socket close')
+        }
+        socket.current.onerror = () => {
+            console.log('Socket error')
+        }
+    }
+
+    const sendMessage = async () => {
+        const message = {
+            username,
+            message: value,
+            id: Date.now(),
+            event: 'message'
+        }
+        socket.current.send(JSON.stringify(message));
+        setValue('')
+    }
+
+
+    if (!connected) {
+        return (
+            <div className="center">
+                <div className="form">
+                    <input
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        type="text"
+                        placeholder="Enter name" />
+                    <button onClick={connect}>Log In</button>
+                </div>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text" />
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+                <div className="messages">
+                    {messages.map(mess =>
+                        <div key={uniqid()}>
+                            {mess.event === 'connection'
+                                ? <div className="connection_message">
+                                    User {mess.username} connect
+                                </div>
+                                : <div className="message">
+                                    {mess.username}. {mess.message}
+                                </div>
+                            }
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WebSock;
